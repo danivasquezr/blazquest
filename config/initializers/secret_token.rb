7ba4f64fd0cdec0b08dfefdf9919ff6649f21d4c
Daniel <danivasquezr@gmail.com>
2014-11-24 23:25:26 -0300

    rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blazquest::Application.config.secret_key_base = '1d3f0441e6c3e3b4623e79649850b63ea70f19cb1cfecc0e55e9079d3cfa07ea19c42fb9a9b7e00ed1500d4fe2438a1963dee3ec71848d974c5ce53a34c9f4a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blazquest::Application.config.session_store :cookie_store, key: '_blazquest_session'
